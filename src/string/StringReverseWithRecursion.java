ffa764c34c003a66e1427f3971c6be67b11d18aa
abdellah <selassi.abdellah@gmail.com>
2019-05-03 10:20:19 +0200

    Add String reverse algorithm

--- src/string/StringReverseWithRecursion.java
+++ src/string/StringReverseWithRecursion.java
@@ -1,5 +1,14 @@
 package string;
 
 public class StringReverseWithRecursion {
+    public static void main(String[] args) {
 
+        System.out.println(revert("tsccsx"));
+    }
+
+    static String revert(String input){
+        if(input.length() <= 1) return input;
+        return revert(input.substring(1)) + input.charAt(0);
+    }
 }
+
